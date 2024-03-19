cf090ef09d3aca8aa60155990332717187b47d46
Enginglk222 <engincan_3_@hotmail.com>
2024-03-19 04:01:17 +0300

    remove all files

--- LoginRegister/config/crypto.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-const config = {
-  TOKEN_KEY: crypto.randomBytes(32).toString('hex')
-};
-
-module.exports = config;
--- LoginRegister/middleware/authUser.js
+++
@@ -1,8 +0,0 @@
-const isAuth = (req, res, next) => {
-    const session = req.session.user;
-    if (session) {
-      next(true);
-    }
-    next(false);
-  };
-  
